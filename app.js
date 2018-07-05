5b45e4ea4392f255ba1cb04d4b9711b0aa36d1d3
kuatroka <aaarmani_2000@yahoo.com>
2018-07-05 19:45:56 +0100

    app

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ new Vue({
     el:"#app",
     data:{
         title: "Becoming a Vue ninja!",
-        name: "Braga"
+        name: "Rayu",
+        url: "http://youtube.com",
+        classes: ["one", "two"]
     },
     methods: {
         greet(time) {
+            
             
             return `Hello and good ${time}, ${this.name}`
         }
---
+++ app1.js
@@ -0,0 +1,13 @@
+new Vue({
+    el:"#app",
+    data:{
+        title: "Becoming a Vue ninja!",
+        wage: 10
+    },
+    methods: {
+        changeWage(amount) {
+            this.wage += amount
+        }
+          }
+})
+//dfdfdfdfdf
